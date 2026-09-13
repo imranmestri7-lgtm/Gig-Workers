43b2b38db4f8760a4af3c62aec1569680915e5d4
imranmestri7-lgtm <imranmestri7@gmail.com>
2026-09-13 17:56:09 +0530

    Added api to map

--- src/app/components/DeliveryMap.tsx
+++ src/app/components/DeliveryMap.tsx
@@ -59,14 +59,11 @@ export default function DeliveryMap({
         }}
 
       >
-
-        <TileLayer
-
-          attribution='&copy; OpenStreetMap contributors'
-
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-
-        />
+        
+   <TileLayer
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+/>
 
 
         <Marker position={pickupPosition}>
